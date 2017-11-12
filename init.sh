0230b4ccdb774b8d88ba2bb71be81026d81defa2
SPhill <lebedevfk@gmail.com>
2017-11-12 18:54:28 -0500

    1.9-01

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/default.conf
-#sudo /etc/init.d/gunicorn restart
+sudo rm -rf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/default.conf
+sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
 
 #git clone https://github.com/lkphill/test.git /home/box/web
 #bash /home/box/web/init.sh
+#gunicorn -b 0.0.0.0:8080 hello:application & 
