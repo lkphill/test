b74d7467d3ba9a90db1af3c31a5a6094f6695dfe
SPhill <lebedevfk@gmail.com>
2017-11-11 22:39:29 -0500

    fix v4

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/default.conf
-sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/default.conf
+#sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start
 
 #git clone https://github.com/lkphill/test.git /home/box/web
 #bash /home/box/web/init.sh
