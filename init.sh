2387968694da94dee26d067b84aeadeea99efe65
SPhill <lebedevfk@gmail.com>
2017-11-11 21:08:27 -0500

    Исправления в Init.sh часть 3 + файл конфига

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/default.conf
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
+
+#git clone https://github.com/lkphill/test.git /home/box/web
+#bash /home/box/web/init.sh
